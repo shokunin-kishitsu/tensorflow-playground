96cd126f6b0816e3dbfa19e970ec8314717c3a49
Oleksiy Holubyev <lyosha@gmail.com>
2017-03-27 18:54:43 +0200

    Initialized variables using global_variables_initializer()

--- first_steps/first_steps.py
+++ first_steps/first_steps.py
@@ -25,3 +25,6 @@ W = tf.Variable([.3], tf.float32)
 b = tf.Variable([-.3], tf.float32)
 x = tf.placeholder(tf.float32)
 linear_model = W * x + b
+
+init = tf.global_variables_initializer()
+sess.run(init)
