ea556c3ed9b98cc82c6b6d2beca6ac3b590d753f
Oleksiy Holubyev <lyosha@gmail.com>
2017-03-27 18:59:28 +0200

    Calculating the loss value

--- first_steps/first_steps.py
+++ first_steps/first_steps.py
@@ -30,3 +30,8 @@ init = tf.global_variables_initializer()
 sess.run(init)
 
 print(sess.run(linear_model, {x:[1,2,3,4]}))
+
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
