e312c25b1d2c2c57747bc2ed7af2f5782534b07f
Oleksiy Holubyev <lyosha@gmail.com>
2017-03-27 18:49:54 +0200

    Added some placeholders and ran a session on the adder node with params

--- first_steps/first_steps.py
+++ first_steps/first_steps.py
@@ -10,3 +10,10 @@ print(sess.run([node1, node2]))
 node3 = tf.add(node1, node2)
 print("node3: ", node3)
 print("sess.run(node3): ",sess.run(node3))
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b  # + provides a shortcut for tf.add(a, b)
+
+print(sess.run(adder_node, {a: 3, b:4.5}))
+print(sess.run(adder_node, {a: [1,3], b: [2, 4]}))
