062b0e0697886e2ec8c8ecf5f24c8813d34d9d9d
Oleksiy Holubyev <lyosha@gmail.com>
2017-04-21 14:29:23 +0200

    Running convolutional network

--- mnist_advanced/mnist_conv.py
+++ mnist_advanced/mnist_conv.py
@@ -31,6 +31,7 @@ def main(_):
   mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
   x = tf.placeholder(tf.float32, [None, 784])
+  y_ = tf.placeholder(tf.float32, [None, 10])
 
   W_conv1 = weight_variable([5, 5, 1, 32])
   b_conv1 = bias_variable([32])
@@ -59,6 +60,26 @@ def main(_):
   b_fc2 = bias_variable([10])
 
   y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+  cross_entropy = tf.reduce_mean(
+      tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+  train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+  correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+  sess = tf.InteractiveSession()
+  sess.run(tf.global_variables_initializer())
+
+  for i in range(20000):
+    batch = mnist.train.next_batch(50)
+    if i%100 == 0:
+      train_accuracy = accuracy.eval(feed_dict={
+          x:batch[0], y_: batch[1], keep_prob: 1.0})
+      print("step %d, training accuracy %g"%(i, train_accuracy))
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+  print("test accuracy %g"%accuracy.eval(feed_dict={
+      x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
 
 if __name__ == '__main__':
